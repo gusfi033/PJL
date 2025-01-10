1e37671370845be8324bb06c62ca1d0e04e71be7
Diogo Moreira de Sousa <A045139@ipmaia.pt>
2025-01-10 13:17:59 +0000

    Menu Raw Test

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 using System;
 using UnityEngine.VFX;
 public class Timer : MonoBehaviour
